33108a23b252a086568a45a7e3213464a2329502
doriancheminade <cheminadedorian@gmail.com>
2015-10-27 19:13:18 +0100

    [10]test E2E uslist: ajout d'un cas de test

--- ScrumEnv/src/AppBundle/Tests/Controller/BacklogListControllerTest.php
+++ ScrumEnv/src/AppBundle/Tests/Controller/BacklogListControllerTest.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testlist()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertContains('ID', $crawler->filter('#container tr')->text());
     }
 }
