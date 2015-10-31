885fc991163012c308d83f27f5c2f62398819548
doriancheminade <cheminadedorian@gmail.com>
2015-10-31 14:26:15 +0100

    [19-20-21-22]suppression d une US: ajout de la suppression d une US plus les tests E2E

--- ScrumEnv/src/AppBundle/Controller/RouteController.php
+++ ScrumEnv/src/AppBundle/Controller/RouteController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\US;
 
 class RouteController extends Controller 
 {
@@ -14,25 +15,67 @@ class RouteController extends Controller
     public function backlogAction($owner, $project)
     {
         //phony data for testing purpose
-        $usList = array(
-            array("id" => 1,"description" => "this is a user story", "cost" => 2, "priority" => 1),
-            array("id" => 2,"description" => "this is an other user story for my project", "cost" => 3, "priority" => 2),
-            array("id" => 3,"description" => "and an other", "cost" => 1, "priority" => 1),
-            array("id" => 4,"description" => "yet an other", "cost" => 3, "priority" => 2));
-        /* 
+        /*$usList = array(
+            array("owner" => "bob", "project" => "pacman2015", "id" => 1,"description" => "this is a user story", "cost" => 2, "priority" => 1),
+            array("owner" => "bob", "project" => "pacman2015", "id" => 2,"description" => "this is an other user story for my project", "cost" => 3, "priority" => 2),
+            array("owner" => "bob", "project" => "pacman2015", "id" => 3,"description" => "and an other", "cost" => 1, "priority" => 1),
+            array("owner" => "bob", "project" => "pacman2015", "id" => 4,"description" => "yet an other", "cost" => 3, "priority" => 2));
+        */ 
         $usList = $this->getDoctrine()
-            ->getRepository()
-            ->findyB(
-                array('owner' => $owner),
-                array('project' => $project));
-        */
-        if (!$usList) {
+            ->getRepository('AppBundle:US')
+            ->findBy(
+                array('owner' => $owner,
+                    'project' => $project),
+                array('id' => 'ASC')
+             );
+        
+        if (!$usList) 
+        {
             throw $this->createNotFoundException('No data found for owner: '.$owner.', and project: '.$project);
         }
-        else {
+        else 
+        {
             return $this->render("backlog.html.twig", array("usList" => $usList));
         }
     }
-
+    public function formUSBacklogDelete($USToDelete)
+    {
+        $us = new US();
+        $us->set($USToDelete.owner, 
+            $USToDelete.project, 
+            $USToDelete.id,
+            $USToDelete.description,
+            $USToDelete.cost,
+            $USToDelete.priority);
+        
+        $form =  $this->createFormBuilder($us)
+            ->add('owner', 'hidden')
+            ->add('project', 'hidden')
+            ->add('id', 'hidden')
+            ->add('description', 'hidden')
+            ->add('cost', 'hidden')
+            ->add('priority', 'hidden')
+            ->add('delete', 'submit')
+            ->getForm();
+        return $form;
+    }
+    public function USBacklogDeleteAction($owner, $project, $USId){
+        $em = $this->getDoctrine()->getManager();
+        $us = $this->getDoctrine()->getRepository('AppBundle:US')->findOneBy(
+            array('owner' => $owner,
+                'project' => $project,
+                'id' => $USId)
+        );
+        if (!$us) 
+        {
+            throw $this->createNotFoundException('No data found for owner: '.$owner.', and project: '.$project.' ,and id: '.$USId);
+        }
+        else 
+        {
+            $em->remove($us);
+            $em->flush();
+            return $this->redirect($this->get('router')->generate('backlog', array('owner' => $owner, 'project' => $project)));
+        }
+    }
 }
 
