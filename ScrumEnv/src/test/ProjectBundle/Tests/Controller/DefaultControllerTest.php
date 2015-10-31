2bd739aeaffea35f99438a39801707285d36ea13
khelixx <guillaume.verdugo@gmail.com>
2015-10-31 11:40:47 +0100

    commit to run project

--- ScrumEnv/src/test/ProjectBundle/Tests/Controller/DefaultControllerTest.php
+++ ScrumEnv/src/test/ProjectBundle/Tests/Controller/DefaultControllerTest.php
@@ -15,12 +15,4 @@ class DefaultControllerTest extends WebTestCase
         $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
     }
 
-    public function testDelete_Us(){
-
-    	  $client = static::createClient();
-
-    	  $crawler = $client->request('GET', '/Backlog/');
-
-
-    }
 }
