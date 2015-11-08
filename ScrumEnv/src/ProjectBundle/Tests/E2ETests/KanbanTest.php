5ac171d1c49f3a865007398c64f9e961be027c6e
khelixx <guillaume.verdugo@gmail.com>
2015-11-08 16:26:29 +0100

    update test_kanban

--- ScrumEnv/src/ProjectBundle/Tests/E2ETests/KanbanTest.php
+++ ScrumEnv/src/ProjectBundle/Tests/E2ETests/KanbanTest.php
@@ -4,12 +4,12 @@ class KanbanTest extends PHPUnit_Extensions_SeleniumTestCase
   protected function setUp()
   {
     $this->setBrowser("*chrome");
-    $this->setBrowserUrl("http://localhost/conduiteDeProjet/ScrumEnv/web/app_dev.php/ScrumTool/Kanban/bob/pacman2015/visualisation/");
+    $this->setBrowserUrl("http://localhost:8000");
   }
 
   public function testMyTestCase()
   {
-    $this->open("/conduiteDeProjet/ScrumEnv/web/app_dev.php/ScrumTool/Kanban/bob/pacman2015/visualisation/");
+    $this->open("/ScrumTool/Kanban/bob/pacman2015/visualisation/");
     $this->click("link=oG");
     $this->waitForPageToLoad("30000");
     $this->click("link=Done");
