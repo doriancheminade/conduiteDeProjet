1336feaad2bab97aebfb3f378545e0abeeb92fa9
doriancheminade <cheminadedorian@gmail.com>
2015-11-09 13:11:34 +0100

    tests: correction de tests E2E

--- ScrumEnv/src/ProjectBundle/Tests/E2ETests/USListCreationTest.php
+++ ScrumEnv/src/ProjectBundle/Tests/E2ETests/USListCreationTest.php
@@ -6,24 +6,25 @@ class USCreationTest extends PHPUnit_Extensions_SeleniumTestCase
     {
         $this->setBrowser('*firefox');
         $this->setBrowserUrl('http://localhost:8000');
-    }    
+    } 
     public function testCreationOk()
     {    
-        $string = "ceci est une nouvelle US"; 
-        $this->open("/ScrumTool/Backlog/bob/pacman2015/");     
+        $id = "42"; 
+        $this->open("/ScrumTool/Backlog/bob/pacman2015/");
+        $this->waitForPageToLoad("30000");  
+         
         $this->click("css=button.btn.btn-primary");
         $this->waitForPageToLoad("30000");           
         $this->type("id=form_Id", "42");
-        $this->type("id=form_description", $string);
-        $this->type("id=form_priority", "42");
+        $this->type("id=form_description", "ceci est une nouvelle US");
+        $this->type("id=form_priority", $id);
         $this->type("id=form_cost", "24");
         $this->click("css=input.btn.btn-success");
         $this->waitForPageToLoad("30000");
         $this->click("link=Back to backlog");
         $this->waitForPageToLoad("30000");
 
-        $page = $this->getText("id=Project");
-        $this->assertTextPresent($page, $string);
+        $this->assertTextPresent("42");
     }  
 }
 ?>
